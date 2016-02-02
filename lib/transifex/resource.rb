3423a3f85b72c98af036c1e523773773d6ae288e
Cameron Dutro <camertron@gmail.com>
2016-02-02 14:54:13 -0800

    Change category to categories in resource

--- lib/transifex/resource.rb
+++ lib/transifex/resource.rb
@@ -1,11 +1,11 @@
 module Transifex
   class Resource
-    attr_accessor :client, :category, :i18n_type, :source_language_code, :slug, :name
+    attr_accessor :client, :categories, :i18n_type, :source_language_code, :slug, :name
 
     def initialize(project_slug, transifex_data)
       @project_slug = project_slug
       @name = transifex_data[:name]
-      @category = transifex_data[:category]
+      @categories = transifex_data[:categories]
       @i18n_type = transifex_data[:i18n_type]
       @source_language_code = transifex_data[:source_language_code]
       @slug = transifex_data[:slug]
